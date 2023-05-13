6f38d4ec70ed2739bc2c8bd23ccd34db40f05db9
MiinishBatUANL <hector.ortizmu@uanl.edu.mx>
2023-05-13 16:24:51 -0600

    Primer commit

--- app/src/main/java/com/example/idle_rpg_project/EditProfileActivity.kt
+++ app/src/main/java/com/example/idle_rpg_project/EditProfileActivity.kt
@@ -2,11 +2,10 @@ package com.example.idle_rpg_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log as Log1
 
-class MainActivity : AppCompatActivity() {
+class EditProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_edit_profile)
     }
 }
