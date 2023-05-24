7feee37b95ba2bedf26f7ea4afc93f47f884beda
MiinishBatUANL <hector.ortizmu@uanl.edu.mx>
2023-05-24 02:26:40 -0600

    Guild Layouts complete

---
+++ app/src/main/java/com/example/idle_rpg_project/GuildRequestsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.idle_rpg_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GuildRequestsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_guild_requests)
+    }
+}
